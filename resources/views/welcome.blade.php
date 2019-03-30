cc11902dfcd8aef43dd5f7cffb2f2f1f95c0b755
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2019-03-30 08:39:21 -0700

    added bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,12 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
         <title>Scraps</title>
     </head>
     <body>
         <div id="app">
-            <app/>
+            <app></pp>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
